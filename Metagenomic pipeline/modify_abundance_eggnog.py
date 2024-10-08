95d818f52b61019a3b69f61d4e93959d79c98e2e
KuoJian MA <103822339+KJ-Ma@users.noreply.github.com>
2024-10-08 17:18:58 +0800

    Update modify_abundance_eggnog.py

--- Metagenomic pipeline/modify_abundance_eggnog.py
+++ Metagenomic pipeline/modify_abundance_eggnog.py
@@ -1,24 +1,24 @@
-# 打开 merged_RPKM.txt 文件
+# Open the merged_RPKM.txt file
 with open("merged_RPKM.txt", "r") as input_file:
     lines = input_file.readlines()
 
-# 创建一个新的处理后的文件
+# Create a new processed output
 output_lines = []
 for line in lines:
     columns = line.strip().split('\t')
     
-    # 1. 将表格数据中的“-”替换为空值
+    # 1. Replace "-" in table data with empty values
     for i in range(len(columns)):
         if columns[i] == "-":
             columns[i] = ""
     
-    # 2. 将第290列的表格数据中，如果包括两个及以上的字母，则只保留第一个字母
+    # 2. In column 290, if there are two or more letters, keep only the first letter
     if len(columns) >= 290:
         column_289 = columns[289]
         if len(column_289) >= 2:
             columns[289] = column_289[0]
     
-    # 3. 将第290列的表格数据中的ko:删去
+    # 3. In column 290, remove "ko:" prefix if present
     if len(columns) >= 293:
         column_292 = columns[292]
         if column_292.startswith("ko:"):
@@ -26,14 +26,13 @@ for line in lines:
     
     output_lines.append('\t'.join(columns) + '\n')
 
-
-# 获取表头并添加到最后四列后面
+# Retrieve header and add to the last four columns
 header = output_lines[0].strip().split('\t')
 header.extend(["COG_category", "Description", "Preferred_name", "KEGG_ko"])
 output_lines[0] = '\t'.join(header) + '\n'
 
-# 将处理后的数据写入新文件
+# Write the processed data to a new file
 with open("merged_RPKM_eggnog.txt", "w") as output_file:
     output_file.writelines(output_lines)
 
-print("文件处理完成，结果保存在 merged_RPKM_eggnog.txt 文件中。")
+print("File processing complete. Results saved in merged_RPKM_eggnog.txt.")
