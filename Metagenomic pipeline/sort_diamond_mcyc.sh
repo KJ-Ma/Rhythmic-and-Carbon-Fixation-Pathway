7172a75a9d1a7416404eb77714adce5dac1d0e23
KuoJian MA <103822339+KJ-Ma@users.noreply.github.com>
2024-10-08 17:20:24 +0800

    Update sort_diamond_mcyc.sh

--- Metagenomic pipeline/sort_diamond_mcyc.sh
+++ Metagenomic pipeline/sort_diamond_mcyc.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-# 指定输入文件和map文件的名称
+# Specify the names of the input file and map file
 input_file="diamond_mcyc.tsv"
 map_file="/home/adm/database/Mcyc/id2gene.map"
 
-# 检查输入文件和map文件是否存在
+# Check if the input file and map file exist
 if [ ! -f "$input_file" ]; then
-    echo "输入文件 $input_file 不存在."
+    echo "Input file $input_file does not exist."
     exit 1
 fi
 
 if [ ! -f "$map_file" ]; then
-    echo "Map文件 $map_file 不存在."
+    echo "Map file $map_file does not exist."
     exit 1
 fi
 
-# 创建新文件的名称
+# Create the name for the new output file
 output_file="diamond_mcyc_sorted.tsv"
 
-# 提取文件的前两列，并将第三列从map文件中添加到新文件，两列之间只有一个制表符分隔
+# Extract the first two columns from the input file, adding the third column from the map file to the new file
 awk -F'\t' 'NR==FNR {map[$1]=$2; next} map[$2]!="" {print $1 "\t" map[$2]}' "$map_file" "$input_file" > "$output_file"
 
-# 添加表头
+# Add header to the output file
 sed -i '1i gene_name\tmcyc_result' "$output_file"
 
-echo "操作完成，结果保存在 $output_file 中."
+echo "Operation completed. Results saved in $output_file."
